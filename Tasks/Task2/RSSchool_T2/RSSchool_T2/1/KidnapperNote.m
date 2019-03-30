4d7106c418d3917df0c72888330d1d13eba142ff
Vlad M <vlad.maevsky@yandex.ru>
2019-03-31 00:40:11 +0300

    Complete first task

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,25 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+
+    NSMutableArray *magasineArr = [[NSMutableArray alloc] initWithArray:[[magaine lowercaseString] componentsSeparatedByString:@" "]];
+    NSArray *noteArr = [[NSArray alloc] initWithArray:[[note lowercaseString] componentsSeparatedByString:@" "]];
+    BOOL result = YES;
+    
+    for (NSString *str in noteArr) {
+        if ([magasineArr containsObject:str]) {
+            [magasineArr removeObject:str];
+        } else {
+            result = NO;
+        }
+        [str release];
+    }
+    
+    [magasineArr release];
+    [noteArr release];
+    
+    return result;
+}
 @end
