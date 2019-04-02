1bd3db3ff74684d1f38a30e3972135bebd2bf471
Vlad M <vlad.maevsky@yandex.ru>
2019-04-02 11:44:37 +0300

    Complete arabicFromRoman func in exercise 2

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
@@ -4,54 +4,33 @@
 
 - (NSString *)arabicFromRoman:(NSString *)romanString {
 
-//    NSDictionary *num = [[NSDictionary alloc] initWithObjectsAndKeys:@(1), @"I", @(5), @"V", @(10), @"X", @(50), @"L", @(100), @"C", @(500), @"D", @(1000), @"M", nil];
-//
-//    NSMutableArray *arr = [[NSMutableArray alloc] init];
-//    NSInteger result = 0;
-//    for (NSInteger i = 0; i < romanString.length; i++) {
-//        [arr addObject:[NSString stringWithFormat:@"%c", [romanString characterAtIndex:i]]];
-//    }
-//
-//    NSLog(@"--------%@", arr);
-//
-//    for (NSInteger i = 0; i < arr.count; i++) {
-//        NSLog(@"%@", arr[i]);
-//        if (i != arr.count - 1) {
-//            if ([num objectForKey:arr[i]] < [num objectForKey:arr[i + 1]]) {
-//                [[num objectForKey:arr[i+1]] integerValue]
-//            }
-//            if ([arr[i] isEqual: @"I"]) {
-//                NSLog(@"axaxaxa");
-//                result += [[num objectForKey:@"I"] integerValue];
-//            } else if ([arr[i] isEqual: @"V"]) {
-//                result += [[num objectForKey:@"V"] integerValue];
-//            } else if ([arr[i] isEqual: @"X"]) {
-//                result += [[num objectForKey:@"X"] integerValue];
-//            } else if ([arr[i] isEqual: @"L"]) {
-//                result += [[num objectForKey:@"L"] integerValue];
-//            } else if ([arr[i] isEqual: @"C"]) {
-//                result += [[num objectForKey:@"C"] integerValue];
-//            } else if ([arr[i] isEqual: @"D"]) {
-//                result += [[num objectForKey:@"D"] integerValue];
-//            } else if ([arr[i] isEqual: @"M"]) {
-//                result += [[num objectForKey:@"M"] integerValue];
-//            }
-//        }
-//
-//    }
-//
-//    NSLog(@"------%ld", (long)result);
-//    NSLog(@"%@", [num objectForKey:@"I"]);
-    return @"";
+    NSDictionary *num = [[NSDictionary alloc] initWithObjectsAndKeys:@(1), @"I", @(5), @"V", @(10), @"X", @(50), @"L", @(100), @"C", @(500), @"D", @(1000), @"M", nil];
+    NSMutableArray *arr = [[NSMutableArray alloc] init];
+    NSInteger result = 0;
+    
+    for (NSInteger i = 0; i < romanString.length; i++) {
+        [arr addObject:[NSString stringWithFormat:@"%c", [romanString characterAtIndex:i]]];
+    }
+    
+    for (NSInteger i = 0; i < arr.count; i++) {
+        if (i == arr.count - 1) {
+            result += [[num objectForKey:arr[i]] integerValue];
+            continue;
+        }
+        if ([[num objectForKey:arr[i]] integerValue] < [[num objectForKey:arr[i + 1]] integerValue]) {
+            result += [[num objectForKey:arr[i + 1]] integerValue] - [[num objectForKey:arr[i]] integerValue];
+            i++;
+            continue;
+        }
+        if ([[num allKeys] containsObject:arr[i]]) {
+            result += [[num objectForKey:arr[i]] integerValue];
+        }
+    }
+
+    return [NSString stringWithFormat:@"%ld", (long)result];
 }
 
 - (NSString *)romanFromArabic:(NSString *)arabicString {
-    
-//    NSArray *arr = [[NSArray alloc] initWithArray:[arabicString componentsSeparatedByString:@" "]];
-//    
-//    NSLog(@"%@", arr);
-//    
-    
     
     return @"";
 }
